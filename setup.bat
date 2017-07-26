d28361d0b4a7cf75208040e38fb7fc95d7809503
Vitaly Kiselev <vitaly.kiselev@axamit.com>
2017-07-26 12:33:24 +0300

    removed osgi-client generation from setup

--- setup.bat
+++ setup.bat
@@ -47,22 +47,6 @@ cd stub
 call mvn install
 
 
-:: generate osgi bundle project
-cd ..
-call mvn archetype:generate ^
-    -DarchetypeGroupId=org.apache.sling ^
-    -DarchetypeArtifactId=sling-bundle-archetype ^
-    -DgroupId=com.axamit.meetup.osgi ^
-    -DartifactId=osgi-client ^
-    -Dversion=1.0.0 ^
-    -Dpackage=com.axamit.meetup.osgi ^
-    -DappsFolderName=osgi-client ^
-    -DartifactName="osgi-client-name" ^
-    -DpackageGroup="osgi-client-group" ^
-    -DinteractiveMode=false
-
-
-
 :: show elapsed time
 set end=%time%
 echo "setup has been started at %start%"
